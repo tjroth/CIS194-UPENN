8dc60eeb89d50e9cbb6c26cef0ab7c703274fdce
Todd Roth <toddjroth@gmail.com>
2015-01-29 20:37:33 -0500

    part of hw4

--- hw4/hw4.hs
+++ hw4/hw4.hs
@@ -15,3 +15,4 @@ fun2' :: Integer -> Integer
 fun2' =  sum . filter (even) . takeWhile (/=1) . iterate (\n -> ( if even n then n `div` 2 else (3 * n + 1)))
 
 
+data Tree a = Leaf | Node Integer (Tree a) a (Tree a) deriving (Show, Eq)
