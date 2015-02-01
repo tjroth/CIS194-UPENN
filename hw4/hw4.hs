c0f793acea863926ff853e4043f4eef5cde7d376
Todd Roth <toddjroth@gmail.com>
2015-02-01 16:57:16 -0500

    updated hw4

--- hw4/hw4.hs
+++ hw4/hw4.hs
@@ -1,3 +1,5 @@
+----------------------------------------------------------------
+--Exercise 1
 fun1 :: [Integer] -> Integer
 fun1 [] = 1
 fun1 (x:xs)
@@ -15,26 +17,37 @@ fun2' :: Integer -> Integer
 fun2' =  sum . filter (even) . takeWhile (/=1) . iterate (\n -> ( if even n then n `div` 2 else (3 * n + 1)))
 
 
+----------------------------------------------------------------
+--Exercise 2
 data Tree a = Leaf | Node Integer (Tree a) a (Tree a) deriving (Show, Eq)
 
+foldTree :: [a] -> Tree a
+foldTree = snd . foldr addNode (2, Leaf)
+
+addNode :: a -> (Integer, Tree a) -> (Integer, Tree a) 
 addNode v (d, Leaf) = (d, Node 0 Leaf v Leaf)
 addNode v (d, Node i Leaf v' Leaf) = ((d + 1), Node 1 (Node 0 Leaf v Leaf) v' Leaf)
 addNode v (d, Node i ln v' Leaf) = (d, Node d ln v' (Node 0 Leaf v Leaf))
 addNode v (d, Node i ln@(Node j _ _ _) v' rn@(Node k _ _ _)) | j <= k = (d,  Node i (getNode $ addNode v (d-1,ln)) v' rn)
                                                              | otherwise = (d, Node i ln v' (getNode $ addNode v (d-1,rn)))
-getNode (d, n) = n
-
-foldTree :: [a] -> Tree a
-foldTree = snd . foldr addNode (2, Leaf)
+  where
+    getNode (d, n) = n
 
 
-prettyRootNode (Node v l a r) = "Node " ++ (show v) ++ prettyTree 0 l ++ spaces 2 ++ (show a) ++ prettyTree 0 r
---prettyTree sp (Node v Leaf a Leaf) = addReturn v ++ " (Node " ++ (show v) ++ " Leaf " ++ show a ++ " Leaf) \n"
-prettyTree sp (Node v l a r) = addReturn v ++ spaces (2 + sp) ++ "(Node " ++ (show v) ++ prettyTree (sp+4) l ++ (show a) ++ prettyTree (sp+4) r
---prettyTree sp Leaf = "  Leaf \n"
+
+-- A not so elegent attempt at a pretty printer for the above Tree; still needs work
+
+prettyRootNode' d (Node v l a r) = "Node " ++ (show v) ++ prettyNode 2 l ++ prettyVal 2 a ++ prettyNode 2 r
+
+prettyNode d (Node 1 l a r) = spaces d ++ "(Node " ++ (show 1) ++ prettyNode 0 l ++ " " ++ show a ++ prettyNode 0 r
+prettyNode d (Node 0 l a r) = spaces d ++ " (Node " ++ (show 0) ++ prettyNode d l ++ show a ++ prettyNode d r ++ ")"
+prettyNode d (Node v l a r) = "\n" ++ spaces d ++ "(Node " ++ show v ++ "\n" ++ prettyNode (2 + d) l ++ prettyVal (2+d) a ++ "\n" ++ prettyNode (2 + d) r
+prettyNode d Leaf = " Leaf"
+
+prettyVal d v = "\n" ++ spaces d ++ (show v)
 
 spaces sp = concat $ replicate sp " "
 
-addReturn nv | nv < 2 = ""
+addReturn nv | nv < 1 = ""
              | otherwise = "\n"
 
