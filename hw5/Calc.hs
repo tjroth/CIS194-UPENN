f4e44dca5518f089f4f9363cfb0c208d6901bf78
Todd Roth <toddjroth@gmail.com>
2015-02-05 09:39:16 -0500

    ex 4 of hw5 complete

--- hw5/Calc.hs
+++ hw5/Calc.hs
@@ -36,3 +36,36 @@ instance Expr ExprT where
 
 reify :: ExprT -> ExprT
 reify = id
+
+-----------------------------------------
+-- Exercise 4
+
+instance Expr Integer where
+  lit i = i
+  add i1 i2 = i1 + i2
+  mul i1 i2 = i1 + i2
+
+instance Expr Bool where
+  lit b | b <= 0 = False
+        | otherwise = True
+  add = (||)
+  mul = (&&)
+
+instance Expr MinMax where
+  lit i = MinMax i
+  add (MinMax i) (MinMax i2) = MinMax (max i i2)
+  mul (MinMax i) (MinMax i2) = MinMax (min i i2)
+
+instance Expr Mod7 where
+  lit i = Mod7 $ i `mod` 7        
+  add (Mod7 x) (Mod7 y) = Mod7 ( (x + y) `mod` 7)
+  mul (Mod7 x) (Mod7 y) = Mod7 ( (x * y) `mod` 7)
+
+
+
+newtype MinMax = MinMax Integer deriving (Eq, Show)
+
+newtype Mod7 = Mod7 Integer deriving (Eq, Show)
+
+testExp :: Expr a => Maybe a
+testExp = parseExp lit add mul "(3 * -4) + 5"
