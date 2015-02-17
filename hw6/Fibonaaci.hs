40b828ba2bad5231c1f1414cf447ccfacf745888
Todd Roth <toddjroth@gmail.com>
2015-02-17 12:21:49 -0500

    fix folders

--- hw6/Fibonaaci.hs
+++ hw6/Fibonaaci.hs
@@ -1,4 +1,5 @@
-
+-------------------------------
+--Exercise 1
 fib :: Integer -> Integer
 fib 0 = 0
 fib 1 = 1
@@ -7,5 +8,9 @@ fib n = fib (n-1) + fib (n-2)
 fibs1 :: [Integer]
 fibs1 = map fib [0 .. ]
 
-
+-------------------------------
+--Exercise 2
 fibs2 = [0, 1] ++ [ fibs2!!(n-1) + fibs2!!(n-2) | n <- [2..] ]
+
+-------------------------------
+--Exercise 3
