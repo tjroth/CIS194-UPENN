7a8817ad99d6dc43fb438133e5cf96b898ab5cbc
Todd Roth <toddjroth@gmail.com>
2015-02-17 12:57:08 -0500

    ex 3 hw6

--- hw6/Fibonaaci.hs
+++ hw6/Fibonaaci.hs
@@ -14,3 +14,12 @@ fibs2 = [0, 1] ++ [ fibs2!!(n-1) + fibs2!!(n-2) | n <- [2..] ]
 
 -------------------------------
 --Exercise 3
+data Stream a = Stream a (Stream a)
+
+streamToList :: Stream a -> [a]
+streamToList (Stream v s) = v : (streamToList s)
+
+instance Show a => Show (Stream a) where
+  show s = show $ take 10 $ streamToList s
+
+sampleStream a = Stream (a) $ sampleStream (a + 1)
